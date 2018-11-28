a7c5f21ecc1c35a5f3e22d93412f3436da57e9e4
Ray Diaz <rdiaz@tendrilinc.com>
2018-11-28 11:00:32 -0700

    Gzip reading and deserialization working. Postgres insertions next

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,34 @@ use std::io::prelude::*;
 
 #[derive(Debug,Deserialize)]
 struct Trade {
-    id: i64, 
+    time: i64, 
     price: f32,
     amount: f64
 }
 
-fn some() -> Result<(), Box<Error>> {
-    let mut rdr = csv::Reader::from_path("src/resources/test.csv.gz").expect("wut?");
+fn prepend_header(contents: String) -> Result<String, Box<Error>> {
+    Ok(format!("{}\n{}", "time,price,amount", contents))
+}
+
+
+fn deserialize_trades_from_file_contents(contents: String) -> Result<Vec<Trade>, Box<Error>> {
+    let prepended_contents = prepend_header(contents).unwrap();
+    let mut vec: Vec<Trade> = Vec::new();
+
+    let mut rdr = csv::Reader::from_reader(prepended_contents.as_bytes());
     for result in rdr.deserialize() {
         let trade: Trade = result?;
         println!("{:?}", trade);
+        vec.push(trade);
     }
 
-    Ok(())
+    Ok(vec)
 }
 
 fn read_string_from_gzip_file(filename: String) -> Result<String, Box<Error>> {
     let mut file = File::open(filename).expect("something happened");
     let mut buffer = Vec::new();
-    // let mut buf = String::new();
     file.read_to_end(&mut buffer);
-    // file.read_to_string(&mut buf).expect("huh");
 
     let mut gz = GzDecoder::new(&buffer[..]);
     let mut s = String::new();
@@ -47,9 +54,25 @@ fn main() {
 }
 
 #[test]
+fn deserialize_trades_test() {
+    let contents = read_string_from_gzip_file("./src/resources/test.csv.gz".to_string()).unwrap();
+    assert_eq!(contents.len(), 430);
+
+    let trades = deserialize_trades_from_file_contents(contents).unwrap();
+    assert_ne!(trades.len(), 0);
+}
+
+#[test]
 fn read_gz_test() {
 
     let decomped = read_string_from_gzip_file("./src/resources/test.csv.gz".to_owned()).unwrap();
     println!("{:?}",decomped);
     assert_eq!(decomped.len(), 430);
+}
+
+#[test]
+fn prepend_header_test() {
+
+    let prepended = prepend_header("These, are, test\ncontents, which, look\nquite, real, actually\n".to_string()).unwrap();
+    println!("{:?}", prepended);
 }
