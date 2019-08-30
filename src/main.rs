f30bc40291bd2aed42457bae220f6a1fe84d880e
Ray Diaz <rdiaz@tendrilinc.com>
2019-08-29 18:12:36 -0600

    Consumer/Producer Thread Split

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 extern crate postgres;
 extern crate flate2;
-extern crate csv;
-#[macro_use] 
+extern crate futures;
+extern crate crossbeam_channel;
+extern crate crossbeam_utils;
+
+#[macro_use]
 extern crate quicli;
 #[macro_use]
 extern crate serde_derive;
 
+use std::fmt;
 use flate2::read::GzDecoder;
 use std::fs::File;
 use std::error::Error;
 use std::io::BufReader;
 use std::io;
+use crossbeam_utils::thread;
 use std::process;
 use std::io::prelude::*;
-use std::thread;
 use std::time::Duration;
-
+use futures::sync::oneshot;
 use postgres::{Connection, TlsMode};
 use quicli::prelude::*;
+use crossbeam_channel::unbounded;
+
 
 
 #[derive(Debug, StructOpt)]
@@ -37,22 +43,31 @@ struct Trade {
     amount: f32
 }
 
+impl fmt::Display for Trade {
+
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({},{},{})", self.time, self.price, self.amount)
+        }
+
+}
+
+
 fn prepend_header(contents: String) -> Result<String> {
     Ok(format!("{}\n{}", "time,price,amount", contents))
 }
 
-fn deserialize_trades_from_file_contents(contents: String) -> Result<Vec<Trade>> {
-    let prepended_contents = prepend_header(contents).unwrap();
-    let mut vec: Vec<Trade> = Vec::new();
+// fn deserialize_trades_from_file_contents(contents: String) -> Result<Vec<Trade>> {
+//     let prepended_contents = prepend_header(contents).unwrap();
+//     let mut vec: Vec<Trade> = Vec::new();
 
-    let mut rdr = csv::Reader::from_reader(prepended_contents.as_bytes());
-    for result in rdr.deserialize() {
-        let trade: Trade = result?;
-        vec.push(trade);
-    }
+//     let mut rdr = csv::Reader::from_reader(prepended_contents.as_bytes());
+//     for result in rdr.deserialize() {
+//         let trade: Trade = result?;
+//         vec.push(trade);
+//     }
 
-    Ok(vec)
-}
+//     Ok(vec)
+// }
 
 fn read_string_from_gzip_file(filename: &str) -> Result<String> {
     let mut file = File::open(filename).expect("something happened");
@@ -65,7 +80,7 @@ fn read_string_from_gzip_file(filename: &str) -> Result<String> {
     Ok(s)
 }
 
-fn ins_trades(conn: &Connection, trades: Vec<Trade>, exchange: &str, currency: &str){
+fn ins_trades(conn: &Connection, trades: &Vec<Trade>, exchange: &str, currency: &str){
     for t in trades.iter() {
         conn.execute("INSERT INTO trades (exchange, currency, time, price, amount) VALUES ($1, $2, $3, $4, $5)", &[&exchange, &currency, &t.time, &t.price, &t.amount]).unwrap();
     }
@@ -81,35 +96,45 @@ main!(|args: Cli| {
     let user =  args.db_user.to_owned();
     let hostname =  args.db_hostname.to_owned();
     let port =  args.db_port.to_owned();
-    {
-        let num_of_threads = 4;
-        let the_file = args.filename.to_owned();
-        let contents = read_string_from_gzip_file(&the_file).unwrap();
-        let ts = deserialize_trades_from_file_contents(contents);
-        let act_trade_vec:Vec<Trade> = ts.unwrap().clone();
-        {
-            let handle = thread::spawn(move || {
-                let mut trade_it = act_trade_vec.chunks(num_of_threads);
-                let exchange_currency_tuple = extract_exchange_and_currency(&the_file);
-                println!("inserting");
-                trade_it.map(|trade_slices|
-                    ins_trades(&Connection::connect(format!("postgres://{}@{}:{}", user, hostname, port), TlsMode::None).unwrap(), trade_slices.to_vec(), &exchange_currency_tuple.0, &exchange_currency_tuple.1)
-            ).collect::<Vec<_>>();
-            });
-            handle.join().unwrap();
-        }
-    }
+    let the_file = args.filename.to_owned();
+    let exchange_currency_tuple = extract_exchange_and_currency(&the_file);
+
+    let file = File::open(the_file).expect("something happened");
+    let d = GzDecoder::new(file);
+    let (se, r) = unbounded::<Trade>();
+
+    thread::scope(|s| {
+        s.spawn(|_| {
+            for l in io::BufReader::new(d).lines() {
+                let line = l.unwrap();
+                let v: Vec<&str> = line.split(",").collect();
+                let t: Trade = Trade {
+                    time: v.get(0).unwrap().parse::<i64>().unwrap(),
+                    price: v.get(1).unwrap().parse::<f32>().unwrap(),
+                    amount: v.get(2).unwrap().parse::<f32>().unwrap()
+                };
+                se.send(t).unwrap();
+            }
+        });
+        s.spawn(|_| {
+            let conn = &Connection::connect(format!("postgres://{}@{}:{}", user, hostname, port), TlsMode::None).unwrap();
+            loop {
+                let v: Vec<Trade> = r.try_iter().collect();
+                ins_trades(conn, &v, &exchange_currency_tuple.0, &exchange_currency_tuple.1);
+            }
+        });
+    }).unwrap();
+    println!("The end...");
+
+
 });
 
 #[test]
 fn closure_test() {
 
-    let dis_x = |x| format!("{}!", x);
-    let res = dis_x("sup");
-    let do_the_thing = || println!("{}", res);
-    do_the_thing();
-    do_the_thing();
-    do_the_thing();
+    let long_string  = String::from("Hello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, world!!!!!!!!!!!!!Hello, world!");
+    let thang:Vec<&str> = long_string.split(",").collect();
+    println!("{:?}", thang);
 }
 
 #[test]
@@ -125,24 +150,24 @@ fn insert_trades_int_test() {
     let contents = read_string_from_gzip_file(&"./src/resources/test.csv.gz").unwrap();
     assert_eq!(contents.len(), 430);
 
-    let trades = deserialize_trades_from_file_contents(contents).unwrap();
-    assert_ne!(trades.len(), 0);
-    ins_trades(&conn, trades, "an_exchange", "EUR");
-    for c in &conn.query("SELECT count(1) FROM trades", &[]).unwrap() {
-        let cnt: i64 = c.get(0);
-        assert_eq!(cnt, 10);
-    }
-    conn.execute("delete from trades", &[]).unwrap();
+    // let trades = deserialize_trades_from_file_contents(contents).unwrap();
+    // assert_ne!(trades.len(), 0);
+    // ins_trades(&conn, &trades, "an_exchange", "EUR");
+    // for c in &conn.query("SELECT count(1) FROM trades", &[]).unwrap() {
+    //     let cnt: i64 = c.get(0);
+    //     assert_eq!(cnt, 10);
+    // }
+    // conn.execute("delete from trades", &[]).unwrap();
 }
 
-#[test]
-fn deserialize_trades_test() {
-    let contents = read_string_from_gzip_file("./src/resources/test.csv.gz").unwrap();
-    assert_eq!(contents.len(), 430);
+// #[test]
+// fn deserialize_trades_test() {
+//     let contents = read_string_from_gzip_file("./src/resources/test.csv.gz").unwrap();
+//     assert_eq!(contents.len(), 430);
 
-    let trades = deserialize_trades_from_file_contents(contents).unwrap();
-    assert_ne!(trades.len(), 0);
-}
+//     let trades = deserialize_trades_from_file_contents(contents).unwrap();
+//     assert_ne!(trades.len(), 0);
+// }
 
 #[test]
 fn read_gz_test() {
@@ -157,3 +182,25 @@ fn prepend_header_test() {
     let prepended = prepend_header("These, are, test\ncontents, which, look\nquite, real, actually\n".to_string()).unwrap();
     println!("{:?}", prepended);
 }
+
+#[test]
+fn stream_read_test() {
+
+    let filename = "./src/resources/test.csv.gz";
+    let file = File::open(filename).expect("something happened");
+
+    let d = GzDecoder::new(file);
+
+    for l in io::BufReader::new(d).lines() {
+        let line = l.unwrap();
+        let v: Vec<&str> = line.split(",").collect();
+        let t: Trade = Trade {
+             time: v.get(0).unwrap().parse::<i64>().unwrap(),
+             price: v.get(1).unwrap().parse::<f32>().unwrap(),
+             amount: v.get(2).unwrap().parse::<f32>().unwrap()
+        };
+        println!("{}", t);
+    }
+}
+
+
